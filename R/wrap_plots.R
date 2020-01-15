d0193095dc548335bc6b4b56ba3a085a92129911
Kyle Baron <kyleb@metrumrg.com>
2020-01-15 11:26:10 -0600

    update roxygen comment format

--- R/wrap_plots.R
+++ R/wrap_plots.R
@@ -1,38 +1,38 @@
 
-##' Faceted plots
-##'
-##' For these plots, data sets made long with respect to several
-##' y-axis variables and then plotted and faceted with
-##' [ggplot2::facet_wrap].
-##'
-##'
-##' @param df data frame to plot
-##' @param x x-axis data in [col_label] format
-##' @param y y-axis data in [col_label] format
-##' @param ... passed to `fun`
-##' @param fun the plotting function
-##' @param title a title to use for the axis with faceting groups
-##' @param scales passed to `facet_wrap`
-##' @param ncol passed to `facet_wrap`
-##' @param use_labels if `TRUE`, the label part of `col_label` will
-##' be used in the strip; the column name is used otherwise
-##' @param label_fun labeller function; passed to [ggplot::facet_wrap]; the
-##' default is based on [parse_label] and allows latex markup in the label
-##' @param xname placeholder
-##'
-##' @details
-##'
-##' `wrap_cont_cont` is a general function used by the others to
-##' create faceted plots of two continuous variables.  `wrap_cont_time`
-##' plots several continuous variables versus time.  `wrap_res_time`
-##' plots several different residuals (or NPDE) versus time. `wrap_eta_cont`
-##' plots etas versus a continuous covariate. `wrap_hist` creates a faceted
-##' histogram plot.
-##'
-##' @md
-##' @rdname wrap_plots
-##' @name wrap_plots
-##' @export
+#' Faceted plots
+#'
+#' For these plots, data sets made long with respect to several
+#' y-axis variables and then plotted and faceted with
+#' [ggplot2::facet_wrap].
+#'
+#'
+#' @param df data frame to plot
+#' @param x x-axis data in [col_label] format
+#' @param y y-axis data in [col_label] format
+#' @param ... passed to `fun`
+#' @param fun the plotting function
+#' @param title a title to use for the axis with faceting groups
+#' @param scales passed to `facet_wrap`
+#' @param ncol passed to `facet_wrap`
+#' @param use_labels if `TRUE`, the label part of `col_label` will
+#' be used in the strip; the column name is used otherwise
+#' @param label_fun labeller function; passed to [ggplot::facet_wrap]; the
+#' default is based on [parse_label] and allows latex markup in the label
+#' @param xname placeholder
+#'
+#' @details
+#'
+#' `wrap_cont_cont` is a general function used by the others to
+#' create faceted plots of two continuous variables.  `wrap_cont_time`
+#' plots several continuous variables versus time.  `wrap_res_time`
+#' plots several different residuals (or NPDE) versus time. `wrap_eta_cont`
+#' plots etas versus a continuous covariate. `wrap_hist` creates a faceted
+#' histogram plot.
+#'
+#' @md
+#' @rdname wrap_plots
+#' @name wrap_plots
+#' @export
 wrap_cont_cont <- function(df, x, y, ..., fun=cont_cont,
                            title = NULL, scales = "free_y",
                            ncol = NULL, use_labels = FALSE,
@@ -74,26 +74,26 @@ wrap_cont_cont <- function(df, x, y, ..., fun=cont_cont,
     facet_wrap(~variable, scales = scales, ncol = ncol, labeller=label_fun)
 }
 
-##' @rdname wrap_plots
-##' @export
+#' @rdname wrap_plots
+#' @export
 wrap_cont_time <- function(df, ..., x = pm_axis_time()) {
   wrap_cont_cont(df, ..., x = pm_axis_time(), fun = y_time)
 }
 
-##' @rdname wrap_plots
-##' @export
+#' @rdname wrap_plots
+#' @export
 wrap_res_time <- function(df, ..., x = pm_axis_time()) {
   wrap_cont_cont(df, ..., x = x, fun = res_time)
 }
 
-##' @rdname wrap_plots
-##' @export
+#' @rdname wrap_plots
+#' @export
 wrap_eta_cont <- function(df, x, y, scales="fixed", ...) {
   wrap_cont_cont(df, x, y=y, fun = eta_cont, scales = scales, ...)
 }
 
-##' @rdname wrap_plots
-##' @export
+#' @rdname wrap_plots
+#' @export
 wrap_hist <- function(df, x, title =NULL, scales = "free_x", ncol=NULL,
                       use_labels=FALSE, label_fun = label_parse_label, ...) {
   x <- col_labels(x)
@@ -110,8 +110,8 @@ wrap_hist <- function(df, x, title =NULL, scales = "free_x", ncol=NULL,
     facet_wrap(~variable, scales = scales, ncol=ncol, labeller=label_fun)
 }
 
-##' @rdname wrap_plots
-##' @export
+#' @rdname wrap_plots
+#' @export
 wrap_dv_preds <- function(df, ..., title = "Predicted {yname}", xname="", scales="fixed") {
   x <- c(pm_axis_pred(), pm_axis_ipred())
   for(i in seq_along(x)) x[i] <- glue(x[i])
