54e9f153033547e80e3c918d5977df90c1513526
Kyle Baron <kyleb@metrumrg.com>
2020-01-16 06:40:09 -0600

    adds options to dv_pred_ipred #6

--- R/dv_pred_ipred.R
+++ R/dv_pred_ipred.R
@@ -2,37 +2,57 @@
 
 #' @export
 dv_pred_ipred_impl <- function(df,
-                          x = "TIME",
-                          ylab = "Concentration",
-                          xlab = "Time after first dose (hr)",
-                          dv  = "DV",
-                          pred = "PRED",
-                          ipred = "IPRED",
-                          id_col = "USUBJID",
-                          xbreaks = waiver(),
-                          angle = 0,
-                          font_size = 6,
-                          margin = 2,
-                          legend.position = "top",
-                          ncol = NULL,
-                          nrow = NULL,
-                          fun = NULL
-                          ) {
+                               x = pm_axis_time(),
+                               dv  = pm_axis_dv(),
+                               pred = pm_axis_pred(),
+                               ipred = pm_axis_ipred(),
+                               id_col = "USUBJID",
+                               xbreaks = waiver(),
+                               xunit = "hr",
+                               xlab  = NULL,
+                               ylab  = NULL,
+                               angle = 0,
+                               font_size = 6,
+                               margin = 2,
+                               legend.position = "top",
+                               pred_lty = 2,
+                               lwd = 1,
+                               size = 2,
+                               scales = "fixed",
+                               log_y = TRUE,
+                               ncol = NULL,
+                               nrow = NULL,
+                               fun = NULL
+) {
 
   df[["ONES"]] <- 1
   df[["TWOS"]] <- 2
 
-  col <- ggthemes::ptol_pal()(3)
 
-  xl <- xlab
-  yl <- ylab
+  col <- ggthemes::ptol_pal()(3)
+  dv <- col_label(dv)
+  x <- col_label(x)
+  ipred <- col_label(ipred)[1]
+  pred <- col_label(pred)[1]
+  xl <- glue_unit(x[2],xunit)
+  x <- x[1]
+  yl <- dv[2]
+  if(is.character(ylab)) yl <- ylab
+  if(is.character(xlab)) xl <- xlab
+  dv <- dv[1]
+
+  require_numeric(df,x)
+  require_numeric(df,dv)
+  require_numeric(df,pred)
+  require_numeric(df,ipred)
+
   marg <- margin(margin,0,margin,0,unit = "pt")
   fac <- as.formula(paste0("~",id_col))
 
   p <- ggplot(df, aes(x = !!sym(x))) +
-    geom_point(aes(y = DV,shape=factor(ONES)),size=2) +
-    geom_line(aes(y = PRED,col = factor(ONES)),lty=2,lwd=1) +
-    geom_line(aes(y = IPRED,col = factor(TWOS)),lwd=1) +
+    geom_point(aes(y = DV,shape=factor(ONES)),size=size) +
+    geom_line(aes(y = IPRED,col = factor(TWOS)),lwd=lwd) +
+    geom_line(aes(y = PRED,col = factor(ONES)),lty=pred_lty,lwd=lwd) +
     pm_theme() +
     theme(
       legend.position = legend.position,
@@ -41,14 +61,14 @@ dv_pred_ipred_impl <- function(df,
         margin = marg
       )
     ) +
-    facet_wrap(fac,ncol = ncol, nrow = nrow) +
-    scale_y_log10() +
+    facet_wrap(fac,ncol = ncol, nrow = nrow,scales = scales) +
     scale_x_continuous(breaks = xbreaks) +
     ylab(yl) + xlab(xl) +
     ggthemes::scale_color_ptol(name="", labels = c("PRED", "IPRED")) +
     scale_shape_discrete(name = "", solid=FALSE, labels = "DV") +
     rot_x(angle = angle)
-   p
+  if(log_y) p <- p + scale_y_log10()
+  p
 
 }
 
