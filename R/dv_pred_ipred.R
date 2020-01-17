ccb83190e644b46a6e193dbc492ec246df3e9656
Kyle Baron <kyleb@metrumrg.com>
2020-01-17 07:59:15 -0600

    refactor so that dv/pred/ipred are all optional #6

--- R/dv_pred_ipred.R
+++ R/dv_pred_ipred.R
@@ -18,41 +18,79 @@ dv_pred_ipred_impl <- function(df,
                                pred_lty = 2,
                                lwd = 1,
                                size = 2,
+                               dv_line = FALSE,
                                scales = "fixed",
                                log_y = TRUE,
                                ncol = NULL,
                                nrow = NULL,
-                               fun = NULL
-) {
+                               fun = NULL) {
 
   df[["ONES"]] <- 1
   df[["TWOS"]] <- 2
-
-
   col <- ggthemes::ptol_pal()(3)
-  dv <- col_label(dv)
+
+  show_dv <- TRUE
+  show_ipred <- TRUE
+  show_pred <- TRUE
+
+  if(is.null(dv)) {
+    dv <- col_label("DV")
+    yl <- dv[2]
+    dv <- dv[1]
+    show_dv <- FALSE
+  } else {
+    dv <- col_label(dv)
+    yl <- dv[2]
+    dv <- dv[1]
+    require_numeric(df,dv)
+  }
+  if(is.null(ipred)) {
+    ipred <- col_label("IPRED")[1]
+    show_ipred <- FALSE
+  } else {
+    ipred <- col_label(ipred)[1]
+    require_numeric(df,ipred)
+  }
+  if(is.null(pred)) {
+    pred <- col_label("PRED")[1]
+    show_pred <- FALSE
+  } else {
+    pred <- col_label(pred)[1]
+    require_numeric(df,pred)
+  }
+
+
   x <- col_label(x)
-  ipred <- col_label(ipred)[1]
-  pred <- col_label(pred)[1]
   xl <- glue_unit(x[2],xunit)
   x <- x[1]
   yl <- dv[2]
+  require_numeric(df,x)
   if(is.character(ylab)) yl <- ylab
   if(is.character(xlab)) xl <- xlab
-  dv <- dv[1]
-
-  require_numeric(df,x)
-  require_numeric(df,dv)
-  require_numeric(df,pred)
-  require_numeric(df,ipred)
 
   marg <- margin(margin,0,margin,0,unit = "pt")
   fac <- as.formula(paste0("~",id_col))
 
-  p <- ggplot(df, aes(x = !!sym(x))) +
-    geom_point(aes(y = DV,shape=factor(ONES)),size=size) +
-    geom_line(aes(y = IPRED,col = factor(TWOS)),lwd=lwd) +
-    geom_line(aes(y = PRED,col = factor(ONES)),lty=pred_lty,lwd=lwd) +
+  p <- ggplot(df, aes(x = !!sym(x)))
+
+  col_labels <- shape_labels <- character(0)
+
+  if(show_dv) {
+    if(dv_line) p <- p + geom_line(data = df,aes(y=DV), lwd = lwd, col = "darkgrey")
+    p <- p + geom_point(aes(y = DV,shape=factor(ONES)),size=size)
+    shape_labels <- "DV"
+  }
+  if(show_pred) {
+    p <- p + geom_line(aes(y = PRED,col = factor(ONES)),lty=pred_lty,lwd=lwd)
+    col_labels <- "PRED"
+  }
+  if(show_ipred) {
+    p <- p + geom_line(aes(y = IPRED,col = factor(TWOS)),lwd=lwd)
+    col_labels <- c(col_labels,"IPRED")
+  }
+
+  p <-
+    p +
     pm_theme() +
     theme(
       legend.position = legend.position,
@@ -63,10 +101,14 @@ dv_pred_ipred_impl <- function(df,
     ) +
     facet_wrap(fac,ncol = ncol, nrow = nrow,scales = scales) +
     scale_x_continuous(breaks = xbreaks) +
-    ylab(yl) + xlab(xl) +
-    ggthemes::scale_color_ptol(name="", labels = c("PRED", "IPRED")) +
-    scale_shape_discrete(name = "", solid=FALSE, labels = "DV") +
-    rot_x(angle = angle)
+    ylab(yl) + xlab(xl) + rot_x(angle = angle)
+
+  if(length(col_labels) > 0) {
+    p <- p + ggthemes::scale_color_ptol(name="", labels = col_labels)
+  }
+  if(length(shape_labels) > 0) {
+    p <- p + scale_shape_discrete(name = "", solid=FALSE, labels = shape_labels)
+  }
   if(log_y) p <- p + scale_y_log10()
   p
 
