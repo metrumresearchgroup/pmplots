67809545fdc6681cf20a6b2cf7205c289e936308
Kyle Baron <kyleb@metrumrg.com>
2021-05-29 08:32:43 -0500

    documentation

--- R/eta_pairs.R
+++ R/eta_pairs.R
@@ -32,55 +32,55 @@ pairs_upper <- function(data, mapping, ...) {
     )
 }
 
-##' Pairs plots using ggpairs
-##'
-##' This funciton is a wrapper to [GGally::ggpairs] with customized
-##' functions for upper and lower off-diagonal panels. See details for help
-##' on customizing some aspects of the off-diagonal plots.
-##'
-##' @param x plotting data.frame
-##' @param y character `col//label` for pairs data; see [col_label]
-##' @param bins passed to [ggplot2::geom_histogram]
-##' @param alpha passed to [ggplot2::geom_histogram]
-##' @param fill passed to [ggplot2::geom_histogram]
-##' @param col passed to [ggplot2::geom_histogram]
-##' @param label_fun labeler function that gets passed to [GGally::ggpairs];
-##' the default is based on [parse_label] and thus allows latex
-##' expressions in the label (see examples)
-##' @param upper_fun function to use for `upper` argument
-##' @param lower_fun function to use for `lower` argument
-##' @param ... passed to [GGally::ggpairs]
-##'
-##' @details This function requires the `GGally` package to be installed.
-##'
-##' When the length of `etas` is one, arguments are passed to [eta_hist] and
-##' that result is returned.
-##'
-##' Use the options object to modify aspects of the smoother line:
-##' `smooth.col`, `smooth.lwd`, `smooth.lty`, `smooth.method`.  Also, use the
-##' options object to control aspects of the points: `scatter.col`,
-##' `scatter.size`.
-##'
-##' @return The result from a `ggpairs` call (a single plot).
-##'
-##' @examples
-##'
-##' id <- pmplots_data_id()
-##'
-##' etas <- c("ETA1//ETA-CL", "ETA2//ETA-VC", "ETA3//ETA-KA")
-##'
-##' eta_pairs(id,etas)
-##'
-##' df <- data.frame(x = rnorm(1000), y = rnorm(1000))
-##'
-##' pairs_plot(df, c("x", "y"))
-##'
-##' df2 <- dplyr::tibble(x = rnorm(100), y = x^2)
-##'
-##' pairs_plot(df2, c("x//x", "y//x$^2$"))
-##'
-##' @md
-##' @export
+#' Pairs plots using ggpairs
+#'
+#' This funciton is a wrapper to [GGally::ggpairs()] with customized
+#' functions for upper and lower off-diagonal panels. See details for help
+#' on customizing some aspects of the off-diagonal plots.
+#'
+#' @param x plotting data.frame
+#' @param y character `col//label` for pairs data; see [col_label()]
+#' @param bins passed to [ggplot2::geom_histogram()]
+#' @param alpha passed to [ggplot2::geom_histogram()]
+#' @param fill passed to [ggplot2::geom_histogram()]
+#' @param col passed to [ggplot2::geom_histogram()]
+#' @param label_fun labeler function that gets passed to [GGally::ggpairs()];
+#' the default is based on [parse_label()] and thus allows latex
+#' expressions in the label (see examples)
+#' @param upper_fun function to use for `upper` argument
+#' @param lower_fun function to use for `lower` argument
+#' @param ... passed to [GGally::ggpairs()]
+#'
+#' @details This function requires the `GGally` package to be installed.
+#'
+#' When the length of `etas` is one, arguments are passed to [eta_hist()] and
+#' that result is returned.
+#'
+#' Use the options object to modify aspects of the smoother line:
+#' `smooth.col`, `smooth.lwd`, `smooth.lty`, `smooth.method`.  Also, use the
+#' options object to control aspects of the points: `scatter.col`,
+#' `scatter.size`.
+#'
+#' @return The result from a `ggpairs` call (a single plot).
+#'
+#' @examples
+#'
+#' id <- pmplots_data_id()
+#'
+#' etas <- c("ETA1//ETA-CL", "ETA2//ETA-VC", "ETA3//ETA-KA")
+#'
+#' eta_pairs(id,etas)
+#'
+#' df <- data.frame(x = rnorm(1000), y = rnorm(1000))
+#'
+#' pairs_plot(df, c("x", "y"))
+#'
+#' df2 <- dplyr::tibble(x = rnorm(100), y = x^2)
+#'
+#' pairs_plot(df2, c("x//x", "y//x$^2$"))
+#'
+#' @md
+#' @export
 pairs_plot <- function(x, y, bins = 15,
                        alpha = opts$histogram.alpha,
                        fill = opts$histogram.fill,
