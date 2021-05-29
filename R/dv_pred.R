a16eb627273f502738bebb3bbe4abb4d7264812d
Kyle Baron <kyleb@metrumrg.com>
2021-05-29 08:26:36 -0500

    clean up formatting

--- R/dv_pred.R
+++ R/dv_pred.R
@@ -1,50 +1,50 @@
 
-##' Plot DV versus predicted values
-##'
-##' Plots for `DV` versus population or individual predicted values. `dv_preds`
-##' makes both plots and returns them as a list (more in `details`).
-##'
-##' @param df data frame to plot
-##' @param x character name for x-axis data
-##' @param y character name for y-axis data
-##' @param yname used to form y-axis label
-##' @param xname used to form x-axis label
-##' @param ys see [defy()]
-##' @param xs see [defx()]; note that `xs` defaults to `ys` so (by
-##' default) the scale configuration will be identical; pass both `xs` and `ys`
-##' to have them independently configured
-##' @param loglog if `TRUE`, x- and y-axes will be log-transformed
-##' @param scales if `TRUE`, then the x- and y- axes will be forced
-##' to have the same limits
-##' @param logbr when using log scale, should the tick marks be at `full`-log
-##' intervals or `half`-log intervals? If you pass `null`, the default scales
-##' will be used (which might be identical to `full`). Use `xs` and `ys` to
-##' pass custom scales.
-##' @param ... passed to [scatt()] and [layer_as()]
-##'
-##' @details
-##' Since this function creates a scatter plot, both the `x` and `y` columns
-##' must be numeric.
-##'
-##' `dv_preds` returns a list of two plots, with the result of `dv_pred` in the
-##' first position and the result of `dv_ipred` in the second position.  In
-##' this case, `...` are passed to both functions.
-##'
-##' @examples
-##' df <- pmplots_data_obs()
-##'
-##' dv_pred(df)
-##'
-##' dv_ipred(df, yname="MyDrug (ng/mL)")
-##'
-##' dv_preds(df, yname = "MyDrug (ng/mL)")
-##'
-##' @return
-##' `dv_pred` and `dv_ipred` return a single plot; `dv_preds` returns a list
-##' of plots.
-##'
-##' @md
-##' @export
+#' Plot DV versus predicted values
+#'
+#' Plots for `DV` versus population or individual predicted values. `dv_preds`
+#' makes both plots and returns them as a list (more in `details`).
+#'
+#' @param df data frame to plot
+#' @param x character name for x-axis data
+#' @param y character name for y-axis data
+#' @param yname used to form y-axis label
+#' @param xname used to form x-axis label
+#' @param ys see [defy()]
+#' @param xs see [defx()]; note that `xs` defaults to `ys` so (by
+#' default) the scale configuration will be identical; pass both `xs` and `ys`
+#' to have them independently configured
+#' @param loglog if `TRUE`, x- and y-axes will be log-transformed
+#' @param scales if `TRUE`, then the x- and y- axes will be forced
+#' to have the same limits
+#' @param logbr when using log scale, should the tick marks be at `full`-log
+#' intervals or `half`-log intervals? If you pass `null`, the default scales
+#' will be used (which might be identical to `full`). Use `xs` and `ys` to
+#' pass custom scales.
+#' @param ... passed to [scatt()] and [layer_as()]
+#'
+#' @details
+#' Since this function creates a scatter plot, both the `x` and `y` columns
+#' must be numeric.
+#'
+#' `dv_preds` returns a list of two plots, with the result of `dv_pred` in the
+#' first position and the result of `dv_ipred` in the second position.  In
+#' this case, `...` are passed to both functions.
+#'
+#' @examples
+#' df <- pmplots_data_obs()
+#'
+#' dv_pred(df)
+#'
+#' dv_ipred(df, yname="MyDrug (ng/mL)")
+#'
+#' dv_preds(df, yname = "MyDrug (ng/mL)")
+#'
+#' @return
+#' `dv_pred` and `dv_ipred` return a single plot; `dv_preds` returns a list
+#' of plots.
+#'
+#' @md
+#' @export
 dv_pred <- function(df, x = pm_axis_pred(), y = pm_axis_dv(),
                     yname = "value", xname = "value",
                     ys = list(), xs = ys, loglog = FALSE,
@@ -124,15 +124,15 @@ dv_pred <- function(df, x = pm_axis_pred(), y = pm_axis_dv(),
   layer_as(out, ...) + pm_labs(x = xlab, y = ylab)
 }
 
-##' @export
-##' @rdname dv_pred
+#' @export
+#' @rdname dv_pred
 dv_ipred <- function(df, x = pm_axis_ipred(), ...) {
   out <- dv_pred(df, x = x, ...)
   layer_as(out,...)
 }
 
-##' @export
-##' @rdname dv_pred
+#' @export
+#' @rdname dv_pred
 dv_preds <- function(df, ...) {
   list(dv_pred(df, ...), dv_ipred(df, ...))
 }
