467a028876ded84158c2a40a0f53c03789c23fc8
Kyle Baron <kyleb@metrumrg.com>
2024-03-15 23:29:17 -0500

    filter with logical vector rather than data frame

--- R/dv_pred.R
+++ R/dv_pred.R
@@ -92,7 +92,7 @@ dv_pred <- function(df, x = pm_axis_pred(), y = pm_axis_dv(),
   }
 
   if(xs$trans %in% c("log", "log10")) {
-    xkp <- df[,x] > 0
+    xkp <- df[[x]] > 0
     df <- dplyr::filter(df, xkp)
     if(.miss("breaks", inx)) {
       xs$breaks <- log_breaks
@@ -100,7 +100,7 @@ dv_pred <- function(df, x = pm_axis_pred(), y = pm_axis_dv(),
   }
 
   if(ys$trans %in% c("log", "log10")) {
-    ykp <- df[,y] > 0
+    ykp <- df[[y]] > 0
     df <- dplyr::filter(df, ykp)
     if(.miss("breaks", iny)) {
       ys$breaks <- log_breaks
