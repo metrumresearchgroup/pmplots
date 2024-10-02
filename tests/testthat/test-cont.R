6fe253f9301704517a2973a12e865f4c5abaf0bc
Kyle Baron <kyleb@metrumrg.com>
2024-10-02 07:35:48 -0500

    put additional tests downstream of eta_covariate to confirm

--- tests/testthat/test-cont.R
+++ tests/testthat/test-cont.R
@@ -6,3 +6,9 @@ test_that("scatter plot IDs [PMP-TEST-007]", {
   gg <- dv_pred(df, plot_id = TRUE, group = "ID", size = 4)
   expect_is(gg, "gg")
 })
+
+test_that("cont_cont works when receiving integer data for x", {
+  df$AGE <- as.integer(df$AGE)
+  ans <- cont_cont(df, x = "AGE", y = "WT")
+  expect_is(ans, "gg")
+})
