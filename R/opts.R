321a5b5041a69dee72584971c151a27a3e105d02
Kyle Baron <kyleb@metrumrg.com>
2026-04-08 09:07:25 -0500

    Adds tad.unit option

--- R/opts.R
+++ R/opts.R
@@ -54,6 +54,7 @@
 #' @param time.label.short default short time label
 #' @param tafd.label default tafd label
 #' @param tafd.label.short default short tafd label
+#' @param tad.unit default tad unit
 #' @param tad.label default tadd label
 #' @param tad.label.short default tafd label
 #'
@@ -143,6 +144,7 @@ pm_options <- function(smooth.lwd = 1.35,
                        time.label.short = time.label,
                        tafd.label = "Time after first dose", 
                        tafd.label.short = tafd.label,
+                       tad.unit = "h",
                        tad.label = "Time after dose", 
                        tad.label.short = tad.label
                        ) {
